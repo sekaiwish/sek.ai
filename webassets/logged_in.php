caccfb9e45996d3a835f5089e01b61ffd969a0db
Wish <wishdrive.mail@gmail.com>
2016-12-15 17:30:38 +1100

    Logged In page overhauled

--- webassets/logged_in.php
+++ webassets/logged_in.php
@@ -11,36 +11,50 @@ echo('<!DOCTYPE html>
     float: left;
     width: 32%;
     background-color: #333333;
-    transition: background-color 1s;
     display: flex;
     justify-content: center;
     align-items: center;
+    transition: filter 1s, background-color 1s;
   }
   .tile2 {
     float: left;
     width: 49%;
     background-color: #333333;
-    transition: background-color 1s;
     display: flex;
     justify-content: center;
     align-items: center;
+    transition: filter 1s, background-color 1s;
   }
   .tileText {
     font-family: "NeoNoire";
     color: white;
     font-size: 350%;
+    filter: blur(2px);
+    opacity: 0;
+    transition: 1s linear;
   }
   .tile2Text {
     font-family: "NeoNoire";
     color: white;
     text-align: center;
     font-size: 470%;
+    filter: blur(2px);
+    opacity: 0;
+    transition: 1s linear;
   }
   .tile:hover {
     background-color: #777777;
   }
   .tile2:hover {
     background-color: #777777;
+  }
+  .tile:hover > .tileText {
+    filter: blur(0px);
+    opacity: 1;
+  }
+  .tile2:hover > .tile2Text {
+    filter: blur(0px);
+    opacity: 1;
   }
 </style>
 </head>
@@ -54,7 +68,7 @@ echo('<!DOCTYPE html>
 <p class="neonoireTitle">WishDrive</p>
 <p style="text-align:center">
 <div style="height:100%;width:70%;margin:auto">
-  <a href="/FLAC"><div class="tile"><p class="tileText">FLAC</p></div></a>
+  <a href="/FLAC"><div class="tile" style="background-image:url(/webassets/flac.gif);background-size:100% 100%;"><p class="tileText">FLAC</p></div></a>
   <div style="float:left;width:2%;height:30%"><br></div>
   <a href="/anime"><div class="tile"><p class="tileText">ANIME</p></div></a>
   <div style="float:left;width:2%;height:30%"><br></div>
