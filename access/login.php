a909f957168bbfda80886f11018f61ff79d9487b
Wish <wishdrive.mail@gmail.com>
2016-12-19 20:43:34 +1100

    Fixed login bug and added debug script for login

--- access/login.php
+++ access/login.php
@@ -12,10 +12,11 @@ while ($row = mysqli_fetch_array($get, MYSQLI_ASSOC)) {
 }
 mysqli_close($link);
 
+
 $username = strtolower($_POST["username"]);
 $password = $_POST["password"];
 $logincount = count($logins);
-for ($y = 0; $y < $logincount; $y++) {
+for ($y = 1; $y < $logincount; $y++) {
   $usernameLower[$y] = strtolower($logins[$y]["username"]);
   if ($usernameLower[$y] == $username) {
     if ($password == $logins[$y]["password"]) {
@@ -32,4 +33,31 @@ for ($y = 0; $y < $logincount; $y++) {
   }
 }
 header("Location: /loginerror");
+
+# DEBUG
+/*
+$username = strtolower($_POST["username"]);
+echo("Set VARusername ($username)<br>");
+$password = $_POST["password"];
+echo("Set VARpassword ($password)<br>");
+$logincount = count($logins);
+echo("Counted logins ($logincount)<br>");
+for ($y = 1; $y < $logincount; $y++) {
+  $usernameLower[$y] = strtolower($logins[$y]["username"]);
+  echo("Set VARusernameLower (".$usernameLower[$y].")<br>");
+  if ($usernameLower[$y] == $username) {
+    if ($password == $logins[$y]["password"]) {
+      #$_SESSION["logged_in"] = TRUE;
+      #$_SESSION["username"] = $logins[$y]["username"];
+      echo("Login success (".$usernameLower[$y].")<br>");
+      exit();
+    } else {
+      echo("Password did not match<br>");
+    }
+  } else {
+    echo("Username did not match<br>");
+  }
+}
+echo("Login failed")
+*/
 ?>
