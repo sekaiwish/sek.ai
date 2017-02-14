8927ccc6b7e93823160ac10176745491b97c83f6
Wish <wishdrive.mail@gmail.com>
2017-02-14 12:14:20 +1100

    Implemented pages and formatted style

--- chan/index.php
+++ chan/index.php
@@ -1,5 +1,14 @@
 <?php include('C:/xampp/htdocs/webassets/default.php');
-error_reporting(0);?>
+error_reporting(0);
+if(isset($_POST['pageNext'])) {
+	header("Location: /chan/?$pageForward");
+	exit();
+}
+if(isset($_POST['pagePrev'])) {
+	header("Location: /chan/?$pageBackward");
+	exit();
+}
+?>
         <title>
             Sekai > 世界chan
         </title>
@@ -22,16 +31,19 @@ error_reporting(0);?>
         </div>
 <?php
   include("C:/xampp/htdocs/access/sql.php");
+	$x = $_SESSION["postsshown"];
   $page = end(explode('?',$_SERVER['REQUEST_URI']));
-  $x = $_SESSION["postsshown"];
   if(is_numeric($page)) {
+    $pageNext = $page + 1;
+    $pagePrev = $page - 1;
     if($page < 2) {
       $offset = 0;
     } else {
-      $offset = $x*$page-$x;
+      $offset = $x * $page - $x;
     }
   } else {
     $offset = 0;
+		$page = 1;
   }
   $threads = "SELECT thread FROM posts WHERE op = 1 ORDER BY thread DESC LIMIT $x OFFSET $offset";
   $threads = mysqli_query($link,$threads);
@@ -41,7 +53,7 @@ error_reporting(0);?>
     $displayThreads[$y] = $thread;
   }
   $threadCount = count($displayThreads) + 1;
-  for($x=1;$x<$threadCount;$x++) {
+  for($x = 1; $x < $threadCount; $x++) {
     $postData[$x] = "SELECT id, name, time, body, filename, filetype, filesize FROM posts WHERE op = 1 AND thread = ".$displayThreads[$x]['thread'];
     $postData[$x] = mysqli_query($link,$postData[$x]);
     $postData[$x] = mysqli_fetch_array($postData[$x],MYSQLI_ASSOC);
@@ -56,7 +68,13 @@ error_reporting(0);?>
         <div class="post">
 ');
     if(isset($postData[$x]['filename'])) {
-      echo('            <a href="/chan/files/'.$postData[$x]['id'].'.'.$postData[$x]['filetype'].'" target="_blank"><img style="float:left;padding-right:15px;padding-bottom:15px;" src="/chan/thumbs/'.$postData[$x]['id'].'.jpg"></a>
+      echo('            <a href="/chan/files/'.
+      $postData[$x]['id'].
+      '.'.
+      $postData[$x]['filetype'].
+      '" target="_blank"><img class="chan" src="/chan/thumbs/'.
+      $postData[$x]['id'].
+      '.jpg"></a>
 ');
     }
     echo("            <p>".
@@ -77,6 +95,42 @@ error_reporting(0);?>
         </div>
 ');
   }
+	$pages = ' [<a href="?1">1</a>]'.
+	' [<a href="?2">2</a>]'.
+	' [<a href="?3">3</a>]'.
+	' [<a href="?4">4</a>]'.
+	' [<a href="?5">5</a>]'.
+	' [<a href="?6">6</a>]'.
+	' [<a href="?7">7</a>]'.
+	' [<a href="?8">8</a>]'.
+	' [<a href="?9">9</a>]'.
+	' [<a href="?10">10</a>]';
+	$pages = explode('[<a href="?'.$page.'">'.$page.'</a>]', "$pages");
+	$pages = $pages[0].'[<a href="?'.$page.'"><b>'.$page.'</b></a>]'.$pages[1];
+  echo('        <div class="pageBox">
+            <div>
+				<a href="?'.$pagePrev.'">
+					<button');
+	if($page == 1) {
+		echo(' disabled');
+	}
+	echo('>Previous</button>
+				</a>
+            </div>
+            <div>
+                <p class="pages">'.$pages.'</p>
+            </div>
+            <div>
+				<a href="?'.$pageNext.'">
+					<button');
+	if($page == 10) {
+		echo(' disabled');
+	}
+	echo('>Next</button>
+				</a>
+            </div>
+        </div>
+');
   mysqli_close($link);
 ?>
     </body>
