17603924fc89aaf56ce5be64397aba674eb4f423
Unknown <wish@sek.ai>
2017-08-10 14:30:43 +1000

    August 10th Update 2
    Added highlights to livesearch results.
    Fixed unencoded URLs being sent to livesearch results.

--- livesearch.php
+++ livesearch.php
@@ -12,6 +12,7 @@ $dir_iterator = new RecursiveDirectoryIterator("$directory/", RecursiveDirectory
 $iterator = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
 $count = 0;
 $list = [];
+/* Use for folder-only search
 foreach($iterator as $file) {
   if ($file->isDir()) {
     $file = substr($file, 1+$directoryLength);
@@ -21,12 +22,26 @@ foreach($iterator as $file) {
     }
   }
 }
+*/
+foreach($iterator as $file) {
+  $file = substr($file, 1+$directoryLength);
+  if (preg_match("/\.php/", $file)) {
+    continue;
+  } else {
+    if (stripos($file, $query) !== false) {
+      $count++;
+      array_push($list, $file);
+    }
+  }
+}
 $results = "";
 foreach ($list as $file) {
+  $fileUrl = rawurlencode(str_replace("\\", "/", $file));
+  $file = implode("<b>$query</b>", explode($query, $file));
   if ($results == "") {
-    $results = "In: $directory<br><a target=\"_blank\" href=\"../$directory/$file/\">$file</a>";
+    $results = "In: $directory<br><a target=\"_blank\" href=\"../$directory/$fileUrl\">$file</a>";
   } else {
-    $results = "$results<br>\n<a target=\"_blank\" href=\"../$directory/$file/\">$file</a>";
+    $results = "$results<br>\n<a target=\"_blank\" href=\"../$directory/$fileUrl\">$file</a>";
   }
 }
 if ($results == "") {
