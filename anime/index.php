f5e23c4e1859ef91caed812e3c3858f3a8f84d41
wishu <wish@sek.ai>
2018-09-03 02:51:45 +1000

    :)

--- anime/index.php
+++ anime/index.php
@@ -10,11 +10,11 @@
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.13/css/brands.css" integrity="sha384-VGCZwiSnlHXYDojsRqeMn3IVvdzTx5JEuHgqZ3bYLCLUBV8rvihHApoA1Aso2TZA" crossorigin="anonymous">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-GVa9GOgVQgOk+TNYXu7S/InPTfSDTtBalSgkgqQ7sCik56N9ztlkoTr2f/T44oKV" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="icon" href="assets/favicon/wish/196.png" type="image/png" sizes="196x196">
-    <link rel="icon" href="assets/favicon/wish/128.png" type="image/png" sizes="128x128">
-    <link rel="icon" href="assets/favicon/wish/96.png" type="image/png" sizes="96x96">
-    <link rel="icon" href="assets/favicon/wish/32.png" type="image/png" sizes="32x32">
-    <link rel="icon" href="assets/favicon/wish/16.png" type="image/png" sizes="16x16">
+    <link rel="icon" href="/assets/favicon/sekai/196.png" type="image/png" sizes="196x196">
+    <link rel="icon" href="/assets/favicon/sekai/128.png" type="image/png" sizes="128x128">
+    <link rel="icon" href="/assets/favicon/sekai/96.png" type="image/png" sizes="96x96">
+    <link rel="icon" href="/assets/favicon/sekai/32.png" type="image/png" sizes="32x32">
+    <link rel="icon" href="/assets/favicon/sekai/16.png" type="image/png" sizes="16x16">
   </head>
   <body>
     <div class="card-columns">
@@ -35,14 +35,11 @@
       </div>
     <?php endforeach; ?>
     </div>
-    <div class="logout">
+    <div class="account btn-group">
       <a class="btn btn-danger" href="/php/logout.php">Logout</a>
+      <a class="btn btn-secondary" href="/home/">Home</a>
     </div>
-    <div class="github">
-      <?php $proc=proc_open("git rev-parse --short HEAD",array(array("pipe","r"),array("pipe","w"),array("pipe","w")),$pipes);$commit=trim(stream_get_contents($pipes[1])); ?><a target="_blank" href="//github.com/Wish495/sekai-php/commit/<?php echo $commit; ?>">
-        <button class="btn btn-dark"><i class="fab fa-github"></i>&nbsp;<?php echo $commit; ?></button>
-      </a>
-    </div>
+    <?php include("../php/commit.php"); ?>
     <div class="copyright">
       <button class="btn btn-dark"><i class="fas fa-copyright"></i> Wish 2016-2018</button>
     </div>
