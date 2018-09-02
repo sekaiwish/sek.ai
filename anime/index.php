c4a6213ddc05c30433e81d3400e40e6e36b38c1b
wishu <wish@sek.ai>
2018-09-03 03:40:48 +1000

    something that works better

--- anime/index.php
+++ anime/index.php
@@ -17,23 +17,25 @@
     <link rel="icon" href="/assets/favicon/sekai/16.png" type="image/png" sizes="16x16">
   </head>
   <body>
-    <div class="card-columns">
+    <div id="animeList" class="accordion">
     <?php
       $files = array_slice(scandir("./"), 2);
       $anime = [];
+      $i = 1;
       foreach ($files as $key) {
         if (is_dir($key)) {
+
           array_push($anime, $key);
         }
       }
       foreach ($anime as $key): ?>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title"><?php echo($key); ?></h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="card bg-dark">
+        <button class="card-header mb-0 btn d-flex collapsed text-white" data-toggle="collapse" data-target="#a<?php echo $i; ?>"><?php echo $key; ?></button>
+        <div id="a<?php echo $i; ?>" class="collapse" data-parent="#animeList">
+          <p class="card-text bg-light">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
-    <?php endforeach; ?>
+    <?php $i++; endforeach; ?>
     </div>
     <div class="account btn-group">
       <a class="btn btn-danger" href="/php/logout.php">Logout</a>
@@ -46,5 +48,8 @@
     <div class="ms">
       <button class="btn btn-dark"><?php $time = explode(' ', microtime()); $finish = $time[1] + $time[0]; echo round(($finish-$start),5) * 1000 . "ms"; ?></button>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>$(document).ready(function(){$("[data-toggle='collapse']").collapse();});</script>
   </body>
 </html>
