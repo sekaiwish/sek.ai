f1cb5a7e8e210b45272080b587a1cac6fff272f0
wishu <wish@sek.ai>
2019-06-27 22:07:22 +1000

    Login home from scratch

--- home/index.php
+++ home/index.php
@@ -1,54 +1,28 @@
-<?php $time = explode(' ', microtime()); $start = $time[1] + $time[0]; session_start(); if (!isset($_SESSION["username"])) { header("Location: /error/401.html"); exit(); } ?>
+<?php if (!isset($_SESSION["username"])) { header("Location: /error/401.html"); exit(); } ?>
 <!doctype html>
 <html>
   <head>
-    <title>&#x4E16;&#x754C; &raquo; Home</title>
-    <?php include("../php/sources.php"); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&#x4E16;&#x754C; &#183; Home</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=M+PLUS+1p:300&amp;subset=japanese">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="icon" href="favicon.ico">
   </head>
   <body>
-    <div class="jumbotron bg-dark text-white">
-      <h6 class="display-4 jp"><img src="/assets/favicon/sekai/apple-touch-icon-120x120.png" height="80">&#x4E16;&#x754C;&#x3078;&#x3088;&#x3046;&#x3053;&#x305D;, <?php echo $_SESSION["username"]; ?>!</h6>
-      <p class="lead">Welcome to Sekai! You are now logged into the domain.<br>Feel free to access the content available to you.</p>
-      <hr class="my-4">
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="card bg-outline-dark text-dark">
-            <div class="card-body">
-              <h5 class="card-title">Anime Collection</h5>
-              <p class="card-text">Collection of Crunchyroll and Blu-ray Disc copies of various anime.</p>
-              <a href="/anime/" class="btn btn-outline-primary jp"><b>/anime/</b></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card bg-outline-dark text-dark">
-            <div class="card-body">
-              <h5 class="card-title">FLAC Collection</h5>
-              <p class="card-text">Collection of lossless audio files, mostly in FLAC format.</p>
-              <a href="/flac/" class="btn btn-outline-danger jp"><b>/flac/</b></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card bg-outline-dark text-dark">
-            <div class="card-body">
-              <h5 class="card-title">Sekai Channel</h5>
-              <p class="card-text">Exclusive invite-only imagebooru for Sekai users.</p>
-              <a href="/chan/" class="btn btn-outline-success jp disabled"><b>Unavailable</b></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card bg-outline-dark text-dark">
-            <div class="card-body">
-              <h5 class="card-title">Sekaiiki</h5>
-              <p class="card-text">Pomf-based file hosting with shortened URL links.</p>
-              <a href="/iki/" class="btn btn-outline-info jp"><b>/iki/</b></a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <canvas id="canvas"></canvas>
+    <div id=body>
+      <h1>世界へようこそ！</h1>
+      <br><a href="/account.php">sek.ai account settings</a><br>
+      <br><a href="/iki/">/iki/ - pomf-powered file hosting</a>
+      <br><a href="/flac/">/flac/ - lossless music collection</a>
+      <br><a href="/anime/">/anime/ - bd and webrip collection</a>
+      <br><a>/chan/ - unavailable</a>
+      <button class="jp" id="button" onclick="window.location.href='/php/logout.php';">&#x30ED;&#x30B0;&#x30A2;&#x30A6;&#x30C8;&nbsp;&nbsp;&#10148;</button>
     </div>
-    <?php include("../php/buttons.php"); ?>
+    <script src="../js/home.js" charset="utf-8"></script>
+    <audio id="player">
+      <source src="theme.flac" type="audio/flac">
+    </audio>
   </body>
 </html>
---
+++ js/home.js
@@ -0,0 +1,60 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var trails = [];
+var dead = [];
+var canvasWidth = window.innerWidth;
+function init() {
+  window.requestAnimationFrame(draw);
+}
+function scale() {
+  var scale = window.innerWidth / canvasWidth;
+  var len = trails.length;
+  for (var i = 0; i < len; i++) {
+    trails[i].position = Math.round(trails[i].position * scale);
+  }
+  canvasWidth = window.innerWidth;
+}
+function draw() {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  if (canvasWidth !== window.innerWidth) {
+    scale();
+  }
+  if (trails[0]) {
+    let len = trails.length;
+    for (var i = 0; i < len; i++) {
+      if (trails[i].age < 0) {
+        dead.push(i);
+      }
+    }
+    for (var i = dead.length - 1; i >= 0; i--)
+      trails.splice(dead[i], 1);
+    dead = [];
+  }
+  var newPixel = {};
+  let trailLength = 60;
+  newPixel.age = window.innerHeight + trailLength;
+  newPixel.position = Math.floor((Math.random() * window.innerWidth) + 1);
+  newPixel.velocity = Math.floor((Math.random() * 5) + 2);
+  trails.push(newPixel);
+  let leng = trails.length;
+  for (var i = 0; i < leng; i++) {
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(trails[i].position, window.innerHeight - trails[i].age + trailLength, 1, 1);
+    var gradient = ctx.createLinearGradient(trails[i].position, window.innerHeight - trails[i].age + trailLength, trails[i].position, window.innerHeight - trails[i].age);
+    gradient.addColorStop(0, "#c06000");
+    gradient.addColorStop(1, "#000");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(trails[i].position, window.innerHeight - trails[i].age, 1, trailLength);
+    trails[i].age -= trails[i].velocity;
+  }
+  window.requestAnimationFrame(draw);
+}
+function play() {
+  audio = document.getElementById("player");
+  audio.volume = 0.1;
+  audio.loop = true;
+  audio.play();
+}
+setTimeout(play, 0);
+init();
