9befc29e59a2e1c2d3c32a33014d4d06c0ee6952
wish <wish@sek.ai>
2019-08-24 04:47:52 +1000

    fix issues with spaces in filenames

--- flac/index.php
+++ flac/index.php
@@ -4,25 +4,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>世界 &#183; /flac/</title>
-    <link rel="stylesheet" href="/css/home.css" type="text/css">
+    <link rel="stylesheet" href="/css/flac.css" type="text/css">
   </head>
   <body>
     <canvas id="canvas"></canvas>
     <div id="body">
-      <h1>/flac/</h1>
+      <h1>/flac/<span><?php echo(str_replace("%20", " ", (substr($_SERVER["REQUEST_URI"], 6)))); ?></span></h1>
       <p>
         <?php
-        $dir = scandir("." . substr($_SERVER["REQUEST_URI"], 5));
-        foreach ($dir as $key => $value) {
+        $dir = str_replace("%20", " ", ("." . substr($_SERVER["REQUEST_URI"], 5)));
+        $list = scandir($dir);
+        foreach ($list as $key => $value) {
           if (in_array($value, array(".", "index.php"))) {
             continue;
           } else {
-            $cdir = ("." . substr($_SERVER["REQUEST_URI"], 5));
-            if (is_dir("$cdir$value")) {
+            if (is_dir("$dir$value")) {
               echo("<a href='$value/'>$value</a><br>");
             } else {
-              $size = filesize($value) / 1024;
-              echo("<a href='$value'>$value - {$size}KB</a><br>");
+              $size = round(filesize($dir . $value) / 1024);
+              if ($size > 1024) {
+                $size = round($size / 1024);
+                echo("<a href='$value'>$value - {$size}MB</a><br>");
+              } else {
+                echo("<a href='$value'>$value - {$size}KB</a><br>");
+              }
             }
           }
         }
