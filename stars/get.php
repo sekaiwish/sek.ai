7af0f2c2ff67d9f5ae19569bf4ed11c5664b5ca2
wish <wish@sek.ai>
2021-03-21 16:01:13 +1100

    test

--- stars/get.php
+++ stars/get.php
@@ -1,8 +1,13 @@
 <?php
 header("Content-Type: application/json");
 include("{$_SERVER["DOCUMENT_ROOT"]}/php/sql.php");
-$query = $dbi->prepare('SELECT * FROM stars WHERE maxTime > ? - 180 ORDER BY minTime');
-$query->bind_param('i', time());
+if ($_GET['p']) {
+  $period = $_GET['p'];
+} else {
+  $period = 180;
+}
+$query = $dbi->prepare('SELECT * FROM stars WHERE maxTime > ? - ? ORDER BY minTime');
+$query->bind_param('ii', time(), $period);
 $query->execute();
 $results = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 echo(json_encode($results));
