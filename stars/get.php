80e6517eb07562f67c994c6efe6c97062762bab6
wish <wish@sek.ai>
2021-03-24 20:20:51 +1100

    global import readded

--- stars/get.php
+++ stars/get.php
@@ -5,6 +5,7 @@ $period = 180;
 if ($_GET['p']) {
   $period = $_GET['p'];
 }
+
 if ($period == 'all') {
   $query = $dbi->prepare('SELECT * FROM stars ORDER BY minTime');
 } else {
@@ -14,34 +15,33 @@ if ($period == 'all') {
 $query->execute();
 $results = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 
-/*
 $cr = curl_init('https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars');
 $header[] = 'Authorization: global';
 curl_setopt($cr, CURLOPT_HTTPHEADER, $header);
 curl_setopt($cr, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($cr);
 curl_close($cr);
-
 $data = json_decode($data, true);
+
 foreach ($data as &$star) {
   unset($star['sharedKey']);
-  if (!in_array($star['world'], array_column($results, 'world'))) {
-    array_push($results);
+  // if remote world already tracked (always, but just in case)
+  if (in_array($star['world'], array_column($results, 'world'))) {
+    $index = array_search($star['world'], array_column($results, 'world'));
+    $remote_delta = $star['maxTime'] - $star['minTime'];
+    $local_delta = $results[$index]['maxTime'] - $results[$index]['minTime'];
+    // remote star newer
+    if ($star['minTime'] > $results[$index]['maxTime']) {
+      $results[$index] = $star;
+    // remote star not new but more precise
+    } elseif ($remote_delta < $local_delta) {
+      $results[$index] = $star;
+    }
   }
 }
-*/
 
-// merging is an issue and currently prefers local data
-// check is needed to see if remote data is newer than local data
-#$stars = array_merge($results, $data);
-
-/*
-$stars = $results;
-usort($stars, function($a, $b) {
+usort($results, function($a, $b) {
   return $a['minTime'] <=> $b['minTime'];
 });
-
-echo(json_encode($stars));
-*/
 
 echo(json_encode($results));
