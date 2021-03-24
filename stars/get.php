a3e8dc0973c8f62131daa9f7abb622e75f8a2a0d
wish <wish@sek.ai>
2021-03-24 17:37:32 +1100

    import star data from global

--- stars/get.php
+++ stars/get.php
@@ -1,10 +1,9 @@
 <?php
 header("Content-Type: application/json");
 include("{$_SERVER["DOCUMENT_ROOT"]}/php/sql.php");
+$period = 180;
 if ($_GET['p']) {
   $period = $_GET['p'];
-} else {
-  $period = 180;
 }
 if ($period == 'all') {
   $query = $dbi->prepare('SELECT * FROM stars ORDER BY minTime');
@@ -14,4 +13,17 @@ if ($period == 'all') {
 }
 $query->execute();
 $results = $query->get_result()->fetch_all(MYSQLI_ASSOC);
-echo(json_encode($results));
+
+$cr = curl_init('https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars');
+$header[] = 'Authorization: global';
+curl_setopt($cr, CURLOPT_HTTPHEADER, $header);
+curl_setopt($cr, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($cr);
+curl_close($cr);
+
+$data = json_decode($data, true);
+foreach ($data as &$star) {
+  unset($star['sharedKey']);
+}
+
+echo(json_encode(array_merge($results, $data)));
