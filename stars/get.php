1ad60482d03aff5e2d1b25548c984e9f06ff4e41
wish <wish@sek.ai>
2021-03-24 19:35:54 +1100

    temp revert global import

--- stars/get.php
+++ stars/get.php
@@ -14,6 +14,7 @@ if ($period == 'all') {
 $query->execute();
 $results = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 
+/*
 $cr = curl_init('https://z9smj03u77.execute-api.us-east-1.amazonaws.com/stars');
 $header[] = 'Authorization: global';
 curl_setopt($cr, CURLOPT_HTTPHEADER, $header);
@@ -28,13 +29,19 @@ foreach ($data as &$star) {
     array_push($results);
   }
 }
+*/
 
 // merging is an issue and currently prefers local data
 // check is needed to see if remote data is newer than local data
 #$stars = array_merge($results, $data);
+
+/*
 $stars = $results;
 usort($stars, function($a, $b) {
   return $a['minTime'] <=> $b['minTime'];
 });
 
 echo(json_encode($stars));
+*/
+
+echo(json_encode($results));
